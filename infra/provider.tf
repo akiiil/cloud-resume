893fb97c2e9784bc4697d0dae865cc1215d86c59
Akil <mmoh0106@student.monash.edu>
2025-06-21 13:48:34 +1000

    test iac

--- infra/provider.tf
+++ infra/provider.tf
@@ -12,11 +12,9 @@ terraform {
 
 provider "aws" {                     # Default provider: ap-southeast-2
   region  = "ap-southeast-2"
-  profile = "akil"                   # 💡 remove if you use env-vars
-}
+  }
 
 provider "aws" {                     # Alias for CloudFront / ACM (us-east-1)
   alias   = "virginia"
   region  = "us-east-1"
-  profile = "akil"
-}
+  }
